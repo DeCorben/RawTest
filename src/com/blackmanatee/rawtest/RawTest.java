62044923b5bbf4ed4bab1d2f4a0aad82b8253c40
DeCorben <decorben@gmail.com>
2019-01-06 14:05:59 -0500

    added fail based echoes

--- src/com/blackmanatee/rawtest/RawTest.java
+++ src/com/blackmanatee/rawtest/RawTest.java
@@ -1,14 +1,28 @@
 package com.blackmanatee.rawtest;
 import java.io.*;
+import java.util.*;
 
 public class RawTest {
-	private static boolean pass;
-	private static int testNum;
+	protected static boolean pass;
+	private static int testNum,phaseNum = 0;
 	protected static int threshold = 1;
+	protected static boolean phaseEcho = false;
+	protected static boolean failEcho = false;
+	private static ArrayList<String> echoBuf;
 	
 	public static void echo(String e, int t) {
 		//an echo that only prints if the test fails
-		if(t == 0 || t >= threshold)
+		if(failEcho){
+			echoBuf.add(e);
+			return;
+		}
+		if(t == 0)
+			System.out.println(e);
+		else if(phaseEcho){
+			if(t >= threshold && phaseNum >= threshold)
+				System.out.println(e);
+		}
+		else if(t >= threshold)
 			System.out.println(e);
 	}
 	
@@ -18,24 +32,28 @@ public class RawTest {
 	
 	public void test() {
 		if(pass)
-			echo("Test number "+testNum+":\t\t\t\tpassed",0);
-		else
-			echo("Test number "+testNum+":\t\t\t\tfailed",0);
+			echo("Test number "+phaseNum+"."+testNum+":\t\t\t\tpassed",0);
+		else{
+			if(failEcho)
+				for(String m:echoBuf){
+					echo(m,0);
+				}
+			echo("Test number "+phaseNum+"."+testNum+":\t\t\t\tfailed",0);
+		}
 		testNum++;
 		resetTest();
 	}
 	
-	public void setTest(int i) {
-		testNum = i;
-	}
-	
 	protected void resetTest() {
 		pass = true;
+		echoBuf = new ArrayList<>();
 	}
 	
 	public void startPhase(String p) {
 		resetTest();
-		echo("Begin phase: "+p,0);
+		phaseNum++;
+		testNum = 1;
+		echo("Begin phase "+phaseNum+": "+p,0);
 	}
 	
 	public boolean treeEquals(File ay, File b){
